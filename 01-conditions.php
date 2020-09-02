19fbabfdefb2cd5b193091b605b77e6e5aaad818
mikhawa <michaeljpitz@gmail.com>
2020-09-02 14:36:22 +0200

    exemple 1 : if

--- 01-conditions.php
+++ 01-conditions.php
@@ -9,7 +9,21 @@
 </head>
 <body>
 <h1>Révision : Les conditions</h1>
+<h1>condition simple: if</h1>
+<p>On verifie si 1 condition est remplie</p>
+<?php
+// on met dans une variable un entier entre 0 et 10 inclus
+$hasard = mt_rand(0,10);
 
+// on va vérifier si le chiffre au hasard est plus petit que 5
+if($hasard<5){
+    // variable entre simple '' n'est pas interprétée (affiche $hasard en tant que texte)
+    echo '$hasard vaut '.
+        // entre "" la variable est interprétée
+        "$hasard et est donc plus petit que 5";
+}
+
+?>
 </body>
 </html>
-<?php
+

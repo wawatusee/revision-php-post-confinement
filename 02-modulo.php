da41bae09c9d8ccdd8d0f1498b0f8a06c7d2f993
mikhawa <michaeljpitz@gmail.com>
2020-09-04 14:49:54 +0200

    exemple utilisation de modulo

--- 02-modulo.php
+++ 02-modulo.php
@@ -1,18 +1,41 @@
-<?php
-
-?><!doctype html>
+<!doctype html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>Explication sur le modulo</title>
 </head>
 <body>
-
+<h1>%</h1>
 <h3>Explication sur le modulo</h3>
-<p>le modulo d'un chiffre nous donne le nombre d'éléments derrière la virgule après la division de celui-ci</p>
+<p>le modulo d'un chiffre nous donne le nombre d'éléments restant de la division euclidienne (derrière la virgule) après la division de celui-ci</p>
+<p>On l'utilise régulièrement avec INT%2 pour savoir si un chiffre est pair ou impaire</p>
+<h4>Exemple d'utilisation du modulo pour l'affichage de données tabulaires</h4>
+<?php
+// création d'un tableau de données que l'on souhaite afficher ligne par ligne
+$donnees = ["Afghanistan","Afrique du Sud","Albanie","Allemagne","Bahreïn","Bangladesh"];
+// on va utiliser le for et non le foreach, on a donc besoin du nombre d'entrée:
+$nb = count($donnees);
+?>
+
+<p>
+    <table align="left">
+    <tr><td>Pays</td></tr>
+    <?php
+    // tant qu'on a des éléments dans le tableau
+    for($i=0;$i<$nb;$i++){
+        // utilisation d'une ternaire pour changer la couleur en cas de ligne paire ou impaire
+        $couleur = ($i%2)?"#FFF":"#FFFF00";
+        // affichage de la ligne avec la couleur
+        echo "<tr><td style='background-color: $couleur'>$donnees[$i]</td></tr>";
+    }
+    ?>
+</table>
+</p>
+<p>
+<br>
 <hr>
 <?php
 $a = 8;
@@ -22,5 +45,6 @@ echo $b." | ".$c;
 echo "<br>".($a/3);
 ?>
 <hr>
+</p>
 </body>
 </html>

ad0676160bdf9f8bde1d68c050919b34b22984f3
mikhawa <michaeljpitz@gmail.com>
2020-09-25 10:53:17 +0200

    premiere()

---
+++ 04-function.php
@@ -0,0 +1,7 @@
+<?php
+
+// création d'une fonction avec le mot clef function suivi du nom, les () sont obligatoires, même si ici nous n'avons pas de paramètre
+function premiere(){
+    // ceci est une variable locale dans laquelle on met le temps du serveur au format 'datetime'
+    $date= date("Y-m-d H:i:s");
+}

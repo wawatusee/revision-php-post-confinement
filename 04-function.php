39d9e4701c737231fddb4e8c01db34ad49543e90
mikhawa <michaeljpitz@gmail.com>
2020-09-25 10:59:19 +0200

    echo premiere()

--- 04-function.php
+++ 04-function.php
@@ -4,4 +4,9 @@
 function premiere(){
     // ceci est une variable locale dans laquelle on met le temps du serveur au format 'datetime'
     $date= date("Y-m-d H:i:s");
+    // pour que ce soit une fonction et pas une procédure, on utilise return pour envoyer un résultat. Le return arrête la fonction
+    return 'La variable $date vaut '."Le $date";
 }
+echo "<hr>";
+echo premiere();
+echo "<hr>";

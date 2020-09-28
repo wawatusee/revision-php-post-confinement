076f0f6872b5f49c120011f3f2c0771a60296267
mikhawa <michaeljpitz@gmail.com>
2020-09-28 10:42:08 +0200

    connectDB.php with die ok

--- 05-fonctions-pratique/model/connectDB.php
+++ 05-fonctions-pratique/model/connectDB.php
@@ -2,5 +2,11 @@
 // procedural mysqli connection
 
 function connectDB(){
-    $connect = mysqli_connect(DB_HOST,DB_USER,DB_PWD,DB_NAME, DB_PORT);
+    $connect = @mysqli_connect(DB_HOST,DB_USER,DB_PWD,DB_NAME, DB_PORT);
+    // error
+    if(mysqli_connect_errno()){
+        die("Erreur : ".mysqli_connect_error()."<br>Numéro d'erreur : ".mysqli_connect_errno());
+    }
+    // change charset
+    mysqli_set_charset($connect,DB_CHARSET);
 }

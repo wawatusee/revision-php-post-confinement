eeeaa3c6b90f51323e29af1d843d9cef509c575e
mikhawa <michaeljpitz@gmail.com>
2020-09-30 16:04:15 +0200

    functionDateModel ok

--- 05-fonctions-pratique/model/functionDateModel.php
+++ 05-fonctions-pratique/model/functionDateModel.php
@@ -3,5 +3,27 @@
 // Argument, un datetime : 2020-09-27 19:26:30
 // résultat de la fonction : Le dimanche 27 septembre 2020 à 19h26
 function functionDateModel($ladate){
-    return "-".$ladate;
+    $string = "le ";
+    // convert to unix time
+    $timeUnix = strtotime($ladate);
+
+    // transtypage error
+    if(!$timeUnix) return "unknow date error";
+
+    // index array with day in french
+    $tab_jour = ["dimanche","lundi","mardi","mercredi","jeudi","vendredi","samedi"];
+
+    $string.= $tab_jour[date("w",$timeUnix)];
+    $string.= " ".date("d",$timeUnix);
+
+    // index array with month in french
+    $tab_mois = ['janvier', 'février', 'mars', 'avril', 'mai', 'juin', 'juillet', 'août', 'septembre', 'octobre', 'novembre', 'décembre'];
+
+    $string .= " ".$tab_mois[date("n",$timeUnix)-1];
+
+    $string .= " ".date("Y",$timeUnix);
+    $string .=" à ";
+    $string .= date("H\hi",$timeUnix);
+
+    return $string;
 }

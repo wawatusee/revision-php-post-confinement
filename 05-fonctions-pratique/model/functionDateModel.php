66d06d581a47d3f0b041e12b6f73b737472c18bd
mikhawa <michaeljpitz@gmail.com>
2020-09-30 14:59:19 +0200

    create functionDateModel() function into model

---
+++ 05-fonctions-pratique/model/functionDateModel.php
@@ -0,0 +1,7 @@
+<?php
+// mettre la date du format datetime vers un format français
+// Argument, un datetime : 2020-09-27 19:26:30
+// résultat de la fonction : Le dimanche 27 septembre 2020 à 19h26
+function functionDateModel($ladate){
+    return $ladate;
+}

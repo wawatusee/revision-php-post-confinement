0126d83902a115349188b8d4498cf3a496ab1aa7
mikhawa <michaeljpitz@gmail.com>
2020-10-02 13:56:12 +0200

    comment

--- 05-fonctions-pratique/model/functionDateModel.php
+++ 05-fonctions-pratique/model/functionDateModel.php
@@ -10,19 +10,25 @@ function functionDateModel($ladate){
     // transtypage error
     if(!$timeUnix) return "unknow date error";
 
-    // index array with day in french
+    // index array with day in french 0->6 US week
     $tab_jour = ["dimanche","lundi","mardi","mercredi","jeudi","vendredi","samedi"];
 
+    // day's of the week (0=>sunday, 1=>monday) 0-6
     $string.= $tab_jour[date("w",$timeUnix)];
+
+    // day of de month 1-31
     $string.= " ".date("d",$timeUnix);
 
-    // index array with month in french
+    // index array with month in french (0->11)
     $tab_mois = ['janvier', 'février', 'mars', 'avril', 'mai', 'juin', 'juillet', 'août', 'septembre', 'octobre', 'novembre', 'décembre'];
 
+    // month of de year - 1 (1->12) => -1
     $string .= " ".$tab_mois[date("n",$timeUnix)-1];
 
+    // year ****
     $string .= " ".date("Y",$timeUnix);
     $string .=" à ";
+    // H : heure \h => (\ permet de ne pas interpréter le caractère qui suit: h (il va l'afficher sans interprétation), i => minutes
     $string .= date("H\hi",$timeUnix);
 
     return $string;

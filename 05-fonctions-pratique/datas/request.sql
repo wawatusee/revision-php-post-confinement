6ff5900ccee2ee2ff1a972344a0ae5794b6ded4d
mikhawa <michaeljpitz@gmail.com>
2020-10-02 14:17:45 +0200

    new query

--- 05-fonctions-pratique/datas/request.sql
+++ 05-fonctions-pratique/datas/request.sql
@@ -1,4 +1,12 @@
+# for articlesLoadAll()
 SELECT * FROM articles a 
+	INNER JOIN users u 
+		ON a.users_idusers = u.idusers
+ORDER BY a.thedate DESC;
+
+# for articlesLoadAllResume() LEFT(table, endpoint) - SUBSTRING or SUBSTR(table, startpoint (1 minimum !), endpoint)
+SELECT a.idarticles, a.titre, LEFT(a.texte,300) AS texte, a.thedate, u.idusers, u.thename 
+FROM articles a 
 	INNER JOIN users u 
 		ON a.users_idusers = u.idusers
 ORDER BY a.thedate DESC;

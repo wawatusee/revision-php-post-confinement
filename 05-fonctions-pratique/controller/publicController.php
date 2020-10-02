4132177c70badb4aaa5ec5d99884cfe82450865a
mikhawa <michaeljpitz@gmail.com>
2020-10-02 14:24:07 +0200

    resume ok

--- 05-fonctions-pratique/controller/publicController.php
+++ 05-fonctions-pratique/controller/publicController.php
@@ -3,7 +3,7 @@
 require_once "model/articlesModel.php";
 require_once "model/functionDateModel.php";
 
-$recupAll = articlesLoadAll($db);
+$recupAll = articlesLoadAllResume($db);
 // pas d'articles
 if(!$recupAll){
     $erreur = "Pas encore d'article";
--- 05-fonctions-pratique/model/articlesModel.php
+++ 05-fonctions-pratique/model/articlesModel.php
@@ -17,7 +17,8 @@ ORDER BY a.thedate DESC;";
 
 // Load all articles with author but with 300 caracters from "texte"
 function articlesLoadAllResume($cdb){
-    $req = "SELECT * FROM articles a 
+    $req = "SELECT a.idarticles, a.titre, LEFT(a.texte,300) AS texte, a.thedate, u.idusers, u.thename 
+FROM articles a 
 	INNER JOIN users u 
 		ON a.users_idusers = u.idusers
 ORDER BY a.thedate DESC;";
